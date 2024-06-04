010d9e0600342ac73965dd375a91beecf9e0f8f0
Valentin Semenov <valentins@MacBook-Pro-Valentin.local>
2024-06-04 18:56:08 +0300

    update: config

--- lua/core/mappings.lua
+++ lua/core/mappings.lua
@@ -3,3 +3,8 @@ vim.g.mapleader = " "
 -- NeoTree
 vim.keymap.set('n', '<leader>e', ':Neotree float focus<CR>')
 vim.keymap.set('n', '<leader>o', ':Neotree float git_status<CR>')
+
+
+-- Add comment to line
+vim.keymap.set('n', 'gc', '<S-i># <Esc>')
+vim.keymap.set('v', 'gcc', '<C-v><S-i># <Esc>')
