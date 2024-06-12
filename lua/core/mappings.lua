524b9d53164b4905bee4d14b583cdab250292356
Valentin Semenov <valentins@MacBook-Pro-Valentin.local>
2024-06-12 20:45:13 +0300

    update config. add mapping key for neotests

--- lua/core/mappings.lua
+++ lua/core/mappings.lua
@@ -13,17 +13,28 @@ vim.keymap.set('n', '<leader>o', ':Neotree float git_status<CR>')
 -- vim.keymap.set('v', 'gcc', '<C-v><S-i># <Esc>')
 
 -- neotest mapping
--- Run test from current file
+-- Запуск всех тестов в рамках тестового файла 
+-- rtf - run test file
 vim.keymap.set('n', '<leader>rtf', ':lua require("neotest").run.run(vim.fn.expand("%"))<CR>')
--- Toggle Summary
-vim.keymap.set('n', '<leader>ts', ':lua require("neotest").summary.toggle()<CR>')
--- Toggle Watch
-vim.keymap.set('n', '<leader>tw', ':lua require("neotest").watch.toggle(vim.fn.expand("%"))<CR>')
--- Show output
+-- Запуск теста, который находиться под курсором
+-- rct - run current test
+vim.keymap.set('n', '<leader>rct', ':lua require("neotest").run.run()<CR>')
+-- Остановка выполнения всех тестов
+-- sat - stop all tests
+vim.keymap.set('n', '<leader>sat', ':lua require("neotest").run.stop()<CR>')
+-- Просмотр результатов теста, курсор должен переместиться внутрь и можно будет перемещаться по результатам тестов
+-- to - test output
 vim.keymap.set('n', '<leader>to', ':lua require("neotest").output.open({ enter = true, auto_close = true})<CR>')
--- Show output panel
+-- Показывает справа колонку с деревом тестов и статусами запускаемых тестов
+vim.keymap.set('n', '<leader>ts', ':lua require("neotest").summary.toggle()<CR>')
+-- Видимо можно следить только за конкретным тестом, пока не разобрался и не использовал
+vim.keymap.set('n', '<leader>tw', ':lua require("neotest").watch.toggle(vim.fn.expand("%"))<CR>')
+-- Открывает панель вывода ниже кода, как будто в отдельном табе, иногда будет полезно
 vim.keymap.set('n', '<leader>t0', ':lua require("neotest").output_panel.toggle()<CR>')
--- Возможно запуск тест под курсором, надо попробовать
-vim.keymap.set('n', '<leader>mr', ':lua require("neotest").run.run()<CR>')
--- vim.api.nvim_set_keymap()
--- vim.keymap.set('n', '<leader>mr', ':lua require("neotest").run.run()<CR>')
+-- Запуск конкретного теста с формированием отчета для allure
+-- twr - test with report
+vim.keymap.set('n', '<leader>twr', ':lua require("neotest").run.run({adapters = {require("neotest-python")({ args = {"--alluredir=/Users/valentins/Desktop/nvim_test_results"}})}})<CR>')
+-- Запуске текущего файла с тестами с формированием отчета для allure
+-- fwr - file with report
+vim.keymap.set('n', '<leader>fwr', ':lua require("neotest").run.run({vim.fn.expand("%"), adapters = {require("neotest-python")({ args = {"-n4", "--alluredir=/Users/valentins/Desktop/nvim_test_results"}})}})<CR>')
+
