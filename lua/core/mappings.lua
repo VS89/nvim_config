f3f923a017813a3af604e67426073c87c1c7528c
Valentin Semenov <valentins@MacBook-Pro-Valentin.local>
2024-06-12 16:32:26 +0300

    update config. add mapping key for neotests

--- lua/core/mappings.lua
+++ lua/core/mappings.lua
@@ -1,5 +1,8 @@
 vim.g.mapleader = " "
 
+-- Сохранить
+
+
 -- NeoTree
 vim.keymap.set('n', '<leader>e', ':Neotree float focus<CR>')
 vim.keymap.set('n', '<leader>o', ':Neotree float git_status<CR>')
@@ -20,5 +23,7 @@ vim.keymap.set('n', '<leader>tw', ':lua require("neotest").watch.toggle(vim.fn.e
 vim.keymap.set('n', '<leader>to', ':lua require("neotest").output.open({ enter = true, auto_close = true})<CR>')
 -- Show output panel
 vim.keymap.set('n', '<leader>t0', ':lua require("neotest").output_panel.toggle()<CR>')
+-- Возможно запуск тест под курсором, надо попробовать
+vim.keymap.set('n', '<leader>mr', ':lua require("neotest").run.run()<CR>')
 -- vim.api.nvim_set_keymap()
 -- vim.keymap.set('n', '<leader>mr', ':lua require("neotest").run.run()<CR>')
